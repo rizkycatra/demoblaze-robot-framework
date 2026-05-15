a67e24462229d56d5e8977d30618e374d97d28fa
rizkycatra <rizky.cp@gmail.com>
2026-05-15 22:35:38 +0700

    fix: remove webdriver_manager call

--- resources/variables/variables.py
+++ resources/variables/variables.py
@@ -2,12 +2,6 @@ import  os
 from dotenv import load_dotenv
 load_dotenv()
 
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-
-def get_chrome_driver_path():
-    return ChromeDriverManager().install()
-
 #env
 URL= "https://www.demoblaze.com"
 BROWSER= "chrome"
